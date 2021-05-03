2a74d1cd393163b64570e997b5fd494d1f80df5f
kirankumar-lab <kiranwebsite63410@gmail.com>
2021-05-03 22:40:00 +0530

    Update Take Attendance Java

--- app/src/main/java/com/pss/myapplication/Utils.java
+++ app/src/main/java/com/pss/myapplication/Utils.java
@@ -4,6 +4,6 @@ package com.pss.myapplication;
 public class Utils {
     // Here pur email id and password of sender
 
-    public static final  String EMAIL = "kiranwebsite63410@gmail.com";
-    public static final String PASSWORD = "edmmodwkebgtdqje";
+    public static final  String EMAIL = "YOUR EMAIL ADDRESS";
+    public static final String PASSWORD = "YOUR PASSWORD";
 }
