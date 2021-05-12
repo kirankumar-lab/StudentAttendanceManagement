b1b70cba55fa9c0699bd1497d667512c2c4035e5
kirankumar-lab <kiranwebsite63410@gmail.com>
2021-05-12 09:27:25 +0530

    Complete View And Edit Attendance

--- app/src/main/java/com/pss/myapplication/TakeAttend.java
+++ app/src/main/java/com/pss/myapplication/TakeAttend.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +30,10 @@ public class TakeAttend extends AppCompatActivity  implements AdapterStudentAtte
     private TextView student_id;
     private boolean isCheck = false;
     private int total;
+
+    private Dialog dialog;
+    private Button btnCancle, btnYes;
+    private TextView tvPresent,tvAbsent,tvTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,14 +94,54 @@ public class TakeAttend extends AppCompatActivity  implements AdapterStudentAtte
                 }
             }
             if (item.getItemId() == R.id.saveAll) {
-                int adid =  db.insertAttendanceDetails(tsid,slot,sem,date,description);
-                if(adid != 0)
-                {
-                    int count=0;
-                    boolean inserted;
-                    for (int i = 0; i < total; i++) {
-                        checkBox = recyclerView.getChildAt(i).findViewById(R.id.isPresent);
-                        student_id = recyclerView.getChildAt(i).findViewById(R.id.student_id);
+                int present = 0;
+                int absent = 0;
+                int totalS = 0;
+
+                for (int i = 0; i < total; i++) {
+                    checkBox = recyclerView.getChildAt(i).findViewById(R.id.isPresent);
+                    if (checkBox.isChecked())
+                    {
+                        present++;
+                    }
+                    else
+                    {
+                        absent++;
+                    }
+                }
+
+                totalS = present + absent;
+
+                dialog = new Dialog(this);
+                dialog.setContentView(R.layout.layout_delete_student);
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.setCancelable(true);
+
+                btnCancle = dialog.findViewById(R.id.btnCancle);
+                btnYes = dialog.findViewById(R.id.btnYes);
+                tvPresent = dialog.findViewById(R.id.tvPresent);
+                tvAbsent = dialog.findViewById(R.id.tvAbsent);
+                tvTotal = dialog.findViewById(R.id.tvTotal);
+                tvPresent.setText(present);
+                tvAbsent.setText(absent);
+                tvTotal.setText(totalS);
+
+                dialog.show();
+
+                btnCancle.setOnClickListener(v -> {
+                    dialog.dismiss();
+                });
+
+                btnYes.setOnClickListener(v -> {
+                    /*---------------------------------------------------Change this Line---------------------------------------------------------------*/
+                    int adid =  db.insertAttendanceDetails(tsid,slot,sem,date,description);
+                    if(adid != 0)
+                    {
+                        int count=0;
+                        boolean inserted;
+                        for (int i = 0; i < total; i++) {
+                            checkBox = recyclerView.getChildAt(i).findViewById(R.id.isPresent);
+                            student_id = recyclerView.getChildAt(i).findViewById(R.id.student_id);
                             inserted = db.insertAttendance(adid,
                                     Integer.parseInt(student_id.getText().toString()),checkBox.isChecked());
                             if(inserted){
@@ -107,21 +153,23 @@ public class TakeAttend extends AppCompatActivity  implements AdapterStudentAtte
                                 count = 0;
                                 break;
                             }
+                        }
+                        if(count==total)
+                        {
+                            dialog.dismiss();
+                            Toast.makeText(this,"Attendance Taked Successfully",Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(this,"Attendance Taked Failed",Toast.LENGTH_SHORT).show();
+                            db.deleteAttendDetail(adid);
+                            db.deleteAttend(adid);
+                            startActivity(new Intent(TakeAttend.this,TakeAttendance.class));
+                            finish();
+                        }
                     }
-                    if(count==total)
-                    {
-                        Toast.makeText(this,"Attendance Taked Successfully",Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                    else
-                    {
-                        Toast.makeText(this,"Attendance Taked Failed",Toast.LENGTH_SHORT).show();
-                        db.deleteAttendDetail(adid);
-                        db.deleteAttend(adid);
-                        startActivity(new Intent(TakeAttend.this,TakeAttendance.class));
-                        finish();
-                    }
-                }
+                });
             }
             return true;
         });
--- app/src/main/java/com/pss/myapplication/Utils.java
+++ app/src/main/java/com/pss/myapplication/Utils.java
@@ -4,6 +4,8 @@ package com.pss.myapplication;
 public class Utils {
     // Here pur email id and password of sender
 
-    public static final  String EMAIL = "YOUR EMAIL ADDRESS";
-    public static final String PASSWORD = "YOUR PASSWORD";
+    //public static final  String EMAIL = "YOUR EMAIL ADDRESS";
+    //public static final String PASSWORD = "YOUR PASSWORD";
+    public static final  String EMAIL = "kiranwebsite63410@gmail.com";
+    public static final String PASSWORD = "edmmodwkebgtdqje";
 }
